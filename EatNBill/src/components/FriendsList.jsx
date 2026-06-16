9921e4f9a7360549a11ea027e74fdff595c48486
ShahzadSyed <syed_shahzad@outlook.com>
2026-06-17 02:09:08 +0500

    component added

--- EatNBill/src/components/FriendsList.jsx
+++ EatNBill/src/components/FriendsList.jsx
@@ -1,27 +1,11 @@
-export default function FriendsList() {
+export default function FriendsList({friends}) {
 
-const dummyFriends = [
-    {
-        name : "Ahmed",
-        image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkliIuVgejvDwavQJbzUFo2z99ptt-UGB43w&s',
-        balance : 0
-    },
-    {
-        name : "Shahzad",
-        image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4nZ136jiwgQJ8Nm-aeU6Hj8QqFI7TsOY-mg&s',
-        balance : 0
-    },
-    {
-        name : "Raheel",
-        image : 'https://www.shutterstock.com/image-photo/close-portrait-smiling-30s-caucasian-260nw-2246095709.jpg',
-        balance : 0
-    }
-]
+
 
     return(
         <div>
             {
-                dummyFriends?.map((df) => <Friends df={df} />)
+                friends?.map((df) => <Friends df={df} />)
             }
         </div>
     )
