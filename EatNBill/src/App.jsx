58788ebd0fdb20a07f9b04abf662f91a86a348b5
ShahzadSyed <syed_shahzad@outlook.com>
2026-06-17 02:07:08 +0500

    main codebase updated

--- EatNBill/src/App.jsx
+++ EatNBill/src/App.jsx
@@ -1,17 +1,35 @@
 import { useState } from 'react'
 import FriendsList from './components/FriendsList.jsx';
+import AddFriends  from './components/AddFriends.jsx';
 import './App.css'
 
 
 function App() {
   
+  const [friends, setfriends] = useState([
+    {
+        name : "Ahmed",
+        image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkliIuVgejvDwavQJbzUFo2z99ptt-UGB43w&s',
+        balance : 0
+    },
+    {
+        name : "Shahzad",
+        image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4nZ136jiwgQJ8Nm-aeU6Hj8QqFI7TsOY-mg&s',
+        balance : 0
+    },
+    {
+        name : "Raheel",
+        image : 'https://www.shutterstock.com/image-photo/close-portrait-smiling-30s-caucasian-260nw-2246095709.jpg',
+        balance : 0
+    }
+])
 
   return (
     <div>
     <h1>Eat N BILL </h1>
-    <FriendsList />
-    {/* <AddFriends />
-    <SplitBill /> */}
+    <FriendsList friends = {friends}/>
+    <AddFriends friends = {friends} setfriends={setfriends} />
+    {/* <SplitBill /> */}
     </div>
   )
 }
