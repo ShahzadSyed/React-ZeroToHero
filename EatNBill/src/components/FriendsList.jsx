307f47ebac545dc5326e43934389c1d9404dddea
ShahzadSyed <syed_shahzad@outlook.com>
2026-06-18 03:01:36 +0500

    components updated

--- EatNBill/src/components/FriendsList.jsx
+++ EatNBill/src/components/FriendsList.jsx
@@ -1,17 +1,17 @@
-export default function FriendsList({friends , setselectedfriend}) {
+export default function FriendsList({friends , setselectedfriend , selectedfriend}) {
 
 
 
     return(
         <div>
             {
-                friends?.map((df) => <Friends df={df} setselectedfriend={setselectedfriend} />)
+                friends?.map((df) => <Friends df={df} selectedfriend={selectedfriend} setselectedfriend={setselectedfriend} />)
             }
         </div>
     )
 }
 
-const Friends = ({df , setselectedfriend}) => {
+const Friends = ({df , setselectedfriend ,selectedfriend }) => {
     return (<div style={{display:"flex", gap: "20px" , alignItems:"center" , marginBottom: "20px"}}>
         <div style={{width : "50px", height : "50px", borderRadius : "50%", overflow: "hidden"}}>
             <img width="100%" height="100%" src={df.image} alt="" />
@@ -20,7 +20,16 @@ const Friends = ({df , setselectedfriend}) => {
             <div>
                 <p>{df.name}</p>
                 <p>Balance :{df.balance}</p>
+                
             </div>
-            <button onClick={() => setselectedfriend(df)}>Select</button>
+            {
+                df.name === selectedfriend?.name ? 
+                <button onClick={() => setselectedfriend()}>Close</button> : 
+                <button onClick={() => setselectedfriend(df)}>Select</button>
+
+                
+
+            }
+            
     </div>)
 }
