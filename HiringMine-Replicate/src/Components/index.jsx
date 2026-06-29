05f2baf6303d723fae57eb2dd69ffb3e6b7136b0
ShahzadSyed <syed_shahzad@outlook.com>
2026-06-30 00:53:38 +0500

    component added

---
+++ HiringMine-Replicate/src/Components/index.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () =>{
+        return(
+            <ul style={{
+                display : 'flex',
+                justifyContent : 'space-around'
+            }}>
+                <Link to="/"> Home</Link>
+                <Link to="/About"> About</Link>
+                <Link to="/Contact"> Contact</Link>
+                
+                
+                {/* <li>Privcy</li>
+                <li>Contact</li> */}
+            </ul>
+        )
+    }
+
+    export default Navbar
