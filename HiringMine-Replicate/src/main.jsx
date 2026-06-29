cc0e1da607ffffe2dec65347acb542404102c6e0
ShahzadSyed <syed_shahzad@outlook.com>
2026-06-30 00:52:54 +0500

    files updated

--- HiringMine-Replicate/src/main.jsx
+++ HiringMine-Replicate/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './Pages/Home/index.jsx'
+import About from './Pages/About/index.jsx'
+import { BrowserRouter, Route , Routes } from 'react-router-dom'
+import Contact from './Pages/Contact/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<App />} />
+    <Route path='/Home' element={<Home />} />
+    <Route path='/About' element={<About />} />
+    <Route path='/Contact' element={<Contact />} />
+  </Routes>  
+  </BrowserRouter>
+  
 )
+
