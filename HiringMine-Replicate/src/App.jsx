7a4e3fb411e16c842cb1f90c90c084d8f006c0bd
ShahzadSyed <syed_shahzad@outlook.com>
2026-07-01 01:14:20 +0500

    codebase updated

--- HiringMine-Replicate/src/App.jsx
+++ HiringMine-Replicate/src/App.jsx
@@ -2,14 +2,114 @@ import { useState } from 'react'
 import Navbar from './Components'
 import './App.css'
 import Home from './Pages/Home'
+import { useEffect } from "react"
 
 function App() {
-  
 
+  const [jobs , setJobs]  =useState([])
+  const [users, setUsers]  =useState([])
+
+  console.log(jobs ,"==>> Jobs")
+  console.log(users ,"==>> users")
+
+
+   function callAPi(url){
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          const {data} = res
+          setJobs(data)
+        })
+        .catch((error) => console.log(error))
+    }
+
+    function callUserAPi(url){
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          const {data} = res
+          setUsers(data)
+        })
+        .catch((error) => console.log(error))
+    }
+
+    useEffect(() => {
+     
+        callAPi('https://api.hiringmine.com/api/jobAds/all?limit=10&pageNo=1&keyWord=&category=&isPending=false&skills=')
+        callUserAPi('https://api.hiringmine.com/api/users/home?sortBy=mostViewed')
+    },[])
+ 
   return (
     <>
+
       <Navbar />
-      <h1>This is App Componenets</h1>
+      <h1 style={{
+        textAlign :"center"
+      }}>This is App Componenets</h1>
+      
+      {
+        jobs.slice(0,3).map(({
+          companyName,
+          designation,
+          payRangeStart,
+          country,
+          city
+        }) => {
+          return(
+            <div style={{
+        border :'2px solid black',
+        textAlign:'center',
+        marginTop:'30px',
+        padding :'20px',
+        display :'flex',
+        flexDirection :'column',
+        gap:'20px'
+      }}>
+        <h1>{companyName}</h1>
+        <h2>{designation}</h2>
+        <h4>salary : {payRangeStart || 'Not Disclosed!'}</h4>
+        <h5>{country}</h5>
+        <h5>{city}</h5>
+      </div>
+          )
+        })
+      }
+
+      {
+        users.slice(0,3).map(({
+          firstName,
+          jobTitle,
+          profilePic,
+          userName,
+          lastName
+        }) => {
+          return(
+            <div style={{
+        border :'2px solid black',
+        textAlign:'center',
+        marginTop:'30px',
+        padding :'20px',
+        display :'flex',
+        flexDirection :'column',
+        gap:'20px',
+        alignItems : 'center'
+      }}>
+        <img style={{
+          width :'100px',
+          height :'100px',
+          borderRadius : '50%'
+        }} src={profilePic} alt="" />
+        <h1>{firstName} {lastName}</h1>
+        <h2>{jobTitle}</h2>
+        <h4>{userName}</h4>
+        
+        
+      </div>
+          )
+        })
+      }
+
+
     </>
   )
 }
