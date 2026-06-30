2e44d07d80a5a5cd3cc01004c41b9b808c984938
ShahzadSyed <syed_shahzad@outlook.com>
2026-07-01 01:17:25 +0500

    components added

---
+++ HiringMine-Replicate/src/Pages/Jobs/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../../Components"
+const Jobs =() => {
+    return(
+        <>
+        <Navbar />
+        <h1 style={{
+            textAlign :'center',
+            marginTop :'20px'
+
+        }}>This is Job Page</h1>
+        </>
+    )
+}
+
+export default Jobs
