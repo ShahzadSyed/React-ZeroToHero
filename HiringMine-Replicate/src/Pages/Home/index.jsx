35a3b5342411c422a17d3b5272484198bf7ec14a
ShahzadSyed <syed_shahzad@outlook.com>
2026-07-01 01:16:22 +0500

    pages updates

--- HiringMine-Replicate/src/Pages/Home/index.jsx
+++ HiringMine-Replicate/src/Pages/Home/index.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react"
 import Navbar from "../../Components"
 
-const Home = () =>{
+const Home = () => {
+
+    function callAPi(url){
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((error) => console.log(error))
+
+
+    }
+
+    useEffect(() => {
+        console.log("useEffect called");
+        callAPi('https://api.hiringmine.com/api/jobAds/all?limit=10&pageNo=1&keyWord=&category=&isPending=false&skills=')
+    },[])
+
+  
+
+
+
     return(
         <>
         <Navbar />
