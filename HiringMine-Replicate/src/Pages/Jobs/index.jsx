138cc7bdb7bcc8f6b710f8cd0bfa99f1702a562d
ShahzadSyed <syed_shahzad@outlook.com>
2026-07-02 03:28:26 +0500

    call filter/Job Api's

--- HiringMine-Replicate/src/Pages/Jobs/index.jsx
+++ HiringMine-Replicate/src/Pages/Jobs/index.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react"
 import Navbar from "../../Components"
+
 const Jobs =() => {
+    
+    const [filters , setFilters] = useState({})
+    const [Jobs , setJobs] = useState({})
+
+    async function callFilterApi(url){
+        const resJSON = await fetch(url)
+        const res = await resJSON.json() //OR => const {data} = await resJSON.json()
+
+        setFilters(res.data) //OR => setFilters(data)
+    }
+
+    async function callJobsApi(url){
+        const resJSON = await fetch(url)
+        const {data} = await resJSON.json() //OR => const {data} = await resJSON.json()
+
+        setJobs(data) //OR => setFilters(data)
+    }
+
+    useEffect(() => {
+
+        //Filters API is called
+        callFilterApi(`https://api.hiringmine.com/api/filterations/all`)
+
+        //Jobs API is called
+        callJobsApi(`https://api.hiringmine.com/api/jobAds/all?limit=10&pageNo=1&keyWord=&category=&isPending=false&skills=`)
+    },[])
+
+
     return(
         <>
         <Navbar />
@@ -8,6 +38,18 @@ const Jobs =() => {
             marginTop :'20px'
 
         }}>This is Job Page</h1>
+
+        <div>
+            <select name="Category" id="">
+                <option value="Category">Category</option>
+                <option value="Developer">Developer</option>
+                <option value="Designer">Designer</option>
+                <option value="Editor">Editor</option>
+                <option value="Marketing">Marketing</option>
+            </select>
+        </div>
+
+        
         </>
     )
 }
