0e9e05714e42751e77bfef181224ac4bc6ef90c3
ShahzadSyed <syed_shahzad@outlook.com>
2026-07-08 02:14:24 +0500

    addeed usememo method

--- memoization/src/App.jsx
+++ memoization/src/App.jsx
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react'
+import { memo, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,20 +6,50 @@ import './App.css'
 
 function App() {
   const [state, setState] = useState(true)
+  // const [count, setCount] = useState(0)
+
+  //here we implement {} != {}
+  const [count, setCount] = useState({
+    value : 0
+  })
+
+  //this obj will re-render, however we are using memo on components, we need to make usememo to stop re-render
+  // const obj = {name : "Shahzad"}   
+
+  const obj = useMemo(() =>{
+    return{
+      name : "Shahzad"
+    }
+  },[])
+
+  // CASE STUDY
+  // 1- memo  => stop re-rendering
+  // 2- if {} != {} means if try to compare empty objects it goes false and memo unable to stop re-rendering
 
   console.log("App is rendering")
 
   return (
     <>
+    {/* <button onClick={() => setCount(count +1)}>Update Count</button> */}
+
+    {/* case 2 = {} != {} */}
+    <button onClick={() => setCount({
+      value : count.value +1
+    })}>Update Count</button>
+
+
     <button onClick={() => setState(!state)}>State Changer</button>
-      <Home />
+
+      <Home count={count} obj ={obj} />
       <About />
       <Contact />
     </>
   )
 }
 
-const Home = memo(() => {
+
+// CASE 1 : MEMOIZATION method for avoiding re-render, until state changed!
+const Home = memo(({count}) => {
   console.log("Home Component is rendering!")
 
   //for checking purpose why we use memo?
@@ -30,7 +60,7 @@ const Home = memo(() => {
 
 
   return(
-    <h1>Hello qworld by Home</h1>
+    <h1>Hello qworld by Home {count.value}</h1>
   )
 })
 
