7f9126ea1549754c28b55a53fe4739fb90e5fb14
ShahzadSyed <syed_shahzad@outlook.com>
2026-07-08 01:41:02 +0500

    memoization Function

--- memoization/src/App.jsx
+++ memoization/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { memo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -7,8 +7,11 @@ import './App.css'
 function App() {
   const [state, setState] = useState(true)
 
+  console.log("App is rendering")
+
   return (
     <>
+    <button onClick={() => setState(!state)}>State Changer</button>
       <Home />
       <About />
       <Contact />
@@ -16,19 +19,30 @@ function App() {
   )
 }
 
-function Home() {
+const Home = memo(() => {
+  console.log("Home Component is rendering!")
+
+  //for checking purpose why we use memo?
+  for (let index = 0; index < 100000; index++) {    
+    console.log(index)
+  }
+
+
+
   return(
     <h1>Hello qworld by Home</h1>
   )
-}
+})
 
 function About() {
+  console.log("About Component is rendering!")
   return(
     <h1>Hello qworld by About</h1>
   )
 }
 
 function Contact() {
+  console.log("Contact Component is rendering!")
   return(
     <h1>Hello qworld by Contact</h1>
   )
